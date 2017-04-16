c26ff5d421174d3839d673091d119921ae4beedc
shadowburn <zjzottawa@gmail.com>
2017-04-16 02:45:08 -0400

    Adding and Fetching reviews done

--- src/test/java/com/jizizhang/review/ApiTest.java
+++ src/test/java/com/jizizhang/review/ApiTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import com.google.gson.Gson;
 
 import com.jizizhang.review.dao.Sql2oCourseDao;
+import com.jizizhang.review.dao.Sql2oReviewDao;
 import com.jizizhang.review.model.Course;
+import com.jizizhang.review.model.Review;
 import com.jizizhang.testing.ApiClient;
 import com.jizizhang.testing.ApiResponse;
 import org.junit.After;
@@ -31,6 +33,7 @@ public class ApiTest {
   private ApiClient client;
   private Gson gson;
   private Sql2oCourseDao courseDao;
+  private Sql2oReviewDao reviewDao;
 
   @BeforeClass
   public static void startServer(){
@@ -47,6 +50,7 @@ public class ApiTest {
   public void setUp() throws Exception {
     Sql2o sql2o = new Sql2o(TEST_DATASOURCE + ";INIT=RUNSCRIPT from 'classpath:db/init.sql'", "", "");
     courseDao = new Sql2oCourseDao(sql2o);
+    reviewDao = new Sql2oReviewDao(sql2o);
     connecton = sql2o.open();
     client = new ApiClient("http://localhost:"+PORT);
     gson = new Gson();
@@ -84,6 +88,45 @@ public class ApiTest {
     ApiResponse res = client.request("GET", "/course/42");
 
     assertEquals(404, res.getStatus());
+  }
+
+  @Test
+  public void addingReviewGivesCreatedStatus() throws Exception {
+    Course course = newTestCourse();
+    courseDao.add(course);
+    Map<String, Object> values = new HashMap<>();
+    values.put("rating", 5);
+    values.put("comment", "test comment");
+
+    ApiResponse res = client.request("POST",
+        String.format("/courses/%d/reviews", course.getId()), gson.toJson(values));
+
+    assertEquals(201, res.getStatus());
+  }
+
+  @Test
+  public void addingReviewToUnknownCourseThrowsError() throws Exception {
+    Map<String, Object> values = new HashMap<>();
+    values.put("rating", 5);
+    values.put("comment", "test comment");
+
+    ApiResponse res = client.request("POST","/courses/100/reviews", gson.toJson(values));
+
+    assertEquals(500, res.getStatus());
+  }
+
+  @Test
+  public void multipleReviewsReturnedForCourse() throws Exception {
+    Course course = newTestCourse();
+    courseDao.add(course);
+    reviewDao.add(new Review(course.getId(), 5, "Test comment 1"));
+    reviewDao.add(new Review(course.getId(), 4, "Test comment 2"));
+
+    ApiResponse res = client.request("GET",
+        String.format("/courses/%d/reviews", course.getId()));
+    Review[] reviews = gson.fromJson(res.getBody(), Review[].class);
+
+    assertEquals(2, reviews.length);
   }
 
   private Course newTestCourse() {
